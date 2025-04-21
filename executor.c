474fbe9fdc402dd337ed62d808593d4f0ea26922
Roullito <jmoleins@laposte.net>
2025-04-21 09:54:03 +0200

    delete pid and ppid printf in the return

--- executor.c
+++ executor.c
@@ -11,13 +11,9 @@ int execute(char **argv, char **env)
 {
 	int status;
 	pid_t pid;
-	pid_t my_pid;
 
 	if (argv == NULL || argv[0] == NULL)
 		return (1);
-
-	my_pid = getppid();
-	printf("PPID: %d\n", my_pid);
 
 	pid = fork();
 	if (pid == -1)
@@ -28,9 +24,6 @@ int execute(char **argv, char **env)
 
 	if (pid == 0)
 	{
-		my_pid = getpid();
-		printf("PID: %d\n", my_pid);
-
 		if (execve(argv[0], argv, env) == -1)
 		{
 			perror("Error: execve");
