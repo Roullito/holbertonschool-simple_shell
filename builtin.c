3b67c04327f765f4ea2f41755bb17625aff263b9
Roullito <jmoleins@laposte.net>
2025-04-22 11:03:36 +0200

    add env in builtin.c and add free in exit builtin

--- builtin.c
+++ builtin.c
@@ -1,19 +1,37 @@
 #include "main.h"
 
 /**
- * handle_builtin - Handles built-in shell commands like "exit"
+ * handle_builtin - Handles built-in shell commands like "exit" and "env"
  * @argv: Array of strings containing the command and its arguments
+ * @env: Array of environment variables
  *
  * Return: -1 if the command is "exit" (to signal termination),
- *         0 otherwise.
+ *          1 if the command is "env" (command handled),
+ *          0 otherwise (not a builtin command).
  */
-int handle_builtin(char **argv)
+int handle_builtin(char **argv, char **env)
 {
-	if (argv[0] && strcmp(argv[0], "exit") == 0)
+	int i;
+	int status = 0;
+
+	if (!argv || !argv[0])
+		return (0);
+
+	if (strcmp(argv[0], "exit") == 0)
 	{
 		if (argv[1])
-			exit(atoi(argv[1]));
+			status = atoi(argv[1]);
+
+		free_argv(argv);
+
+		exit(status);
 		return (-1);
+	}
+	else if (strcmp(argv[0], "env") == 0)
+	{
+		for (i = 0; env[i]; i++)
+			printf("%s\n", env[i]);
+		return (1);
 	}
 
 	return (0);
