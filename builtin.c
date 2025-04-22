61f22c02ff190a0ed370a73d68d6c1b1e449bd82
Roullito <jmoleins@laposte.net>
2025-04-22 11:22:24 +0200

    change handle_builtin with struct to separe exit and env for exemple and ollow us to manage easly if we want to add builtins

--- builtin.c
+++ builtin.c
@@ -1,31 +1,56 @@
 #include "main.h"
 
 /**
- * handle_builtin - Handles built-in shell commands like "exit" and "env"
- * @argv: Array of strings containing the command and its arguments
+ * builtin_exit - Handle the "exit" builtin command
+ * @argv: Array of command arguments
  * @env: Array of environment variables
  *
- * Return: -1 if the command is "exit" (to signal termination),
- *          1 if the command is "env" (command handled),
- *          0 otherwise (not a builtin command).
+ * Return: -1 to indicate shell termination
+ */
+int builtin_exit(char **argv, char **env)
+{
+	(void)argv;
+	(void)env;
+	return (-1);
+}
+
+/**
+ * builtin_env - Handle the "env" builtin command
+ * @argv: Array of command arguments
+ * @env: Array of environment variables
+ *
+ * Return: 1 to indicate the command was handled
+ */
+int builtin_env(char **argv, char **env)
+{
+	int i;
+	(void)argv;
+	for (i = 0; env[i]; i++)
+		printf("%s\n", env[i]);
+	return (1);
+}
+
+/**
+ * handle_builtin - Check and execute builtin shell commands
+ * @argv: Array of command arguments
+ * @env: Array of environment variables
+ *
+ * Return: -1 if "exit", 1 if a builtin was handled, 0 otherwise
  */
 int handle_builtin(char **argv, char **env)
 {
 	int i;
-
+	builtin_t builtins[] = {
+		{"exit", builtin_exit},
+		{"env", builtin_env},
+		{NULL, NULL}
+	};
 	if (!argv || !argv[0])
 		return (0);
-
-	if (strcmp(argv[0], "exit") == 0)
+	for (i = 0; builtins[i].name; i++)
 	{
-		return (-1);
+		if (strcmp(argv[0], builtins[i].name) == 0)
+			return (builtins[i].func(argv, env));
 	}
-	else if (strcmp(argv[0], "env") == 0)
-	{
-		for (i = 0; env[i]; i++)
-			printf("%s\n", env[i]);
-		return (1);
-	}
-
 	return (0);
 }
