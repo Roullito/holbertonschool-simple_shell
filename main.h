eba437c8c5278bfb8897342e33ce8f9a9c657a52
Roullito <jmoleins@laposte.net>
2025-04-23 10:34:04 +0200

    fix handle_builtin_or_execute handle_builtin, builtin_env, builtin_exit to manage exit code return and stock last_status.

--- main.h
+++ main.h
@@ -17,7 +17,7 @@
 typedef struct builtin_s
 {
 	char *name;
-	int (*func)(char **argv, char **env, char *line);
+	int (*func)(char **argv, char **env, char *line, int last_status);
 } builtin_t;
 
 ssize_t _getline(char **lineptr, size_t *n, int fd);
@@ -36,15 +36,15 @@ int _check_direct_path(char *filename);
 char *_extract_path(char **env);
 char *_search_in_path(char *path_str, char *filename);
 int _strncmp(const char *s1, const char *s2, size_t n);
-int handle_builtin(char **argv, char **env, char *line);
+int handle_builtin(char **argv, char **env, char *line, int last_status);
 void free_argv(char **argv);
 int is_interactive(void);
 ssize_t get_input_line(char **line, size_t *len);
 int shell_loop(char **env, char *progname, int *exit_status);
 int handle_builtin_or_execute(char **argv, char **env, char *progname,
-	char *line);
+	char *line, int *exit_status);
 int _is_number(char *str);
-int builtin_exit(char **argv, char **env, char *line);
-int builtin_env(char **argv, char **env, char *line);
+int builtin_exit(char **argv, char **env, char *line, int last_status);
+int builtin_env(char **argv, char **env, char *line, int last_status);
 
 #endif
